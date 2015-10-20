454a6e2b184568d19fb46ded2e9169928ab0ee79
Vino <superdangbo@hotmail.com>
2015-10-20 16:52:59 +0800

    add the file XZero.java

---
+++ src/com/golden/x/main/XZero.java
@@ -0,0 +1,28 @@
+package com.golden.x.main;
+
+public class XZero {
+	  public static void transfer(int d) {
+//        String[] str = { "零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖" };
+        String[] str = { "零", "一", "二", "三", "四", "五", "六", "七", "八", "九" };
+//        String ss[] = new String[] { "元", "拾", "佰", "仟", "万", "拾", "佰", "仟", "亿" };
+//        String ss[] = new String[] { "个", "十", "百", "千", "万", "十", "百", "千", "亿" };
+        String ss[] = new String[] {"", "十", "百", "千", "万", "十", "百", "千", "亿" };
+        String s = String.valueOf(d);
+        System.out.println(s);
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < s.length(); i++) {
+            String index = String.valueOf(s.charAt(i));
+            sb = sb.append(str[Integer.parseInt(index)]);
+        }
+        String sss = String.valueOf(sb);
+        int i = 0;
+        for (int j = sss.length(); j > 0; j--) {
+            sb = sb.insert(j, ss[i++]);
+        }
+        System.out.println(sb);
+    }
+	  
+	public static void main(String[] args) {
+		transfer(987654302);
+	}
+}
